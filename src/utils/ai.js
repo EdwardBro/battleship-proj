f0d4526993194ea3ce77f377dff3a156c609caca
Kuivalainen <d.kuivalainen@yandex.ru>
2023-08-07 15:44:06 +0300

    search matrix

---
+++ src/utils/ai.js
@@ -0,0 +1,44 @@
+const getHardModeSearchMatrix = (n = 4) => {
+    switch (n) {
+        case 2:
+            return [
+                [1, 0], [0, 1]
+            ]
+        case 3: 
+            return [
+                [0, 0, 1],
+                [ 0, 1, 0],
+                [1, 0, 0],
+            ].reverse()
+    
+        default:
+            return [
+                [0, 0, 0, 1],
+                [0, 0, 1, 0],
+                [0, 1, 0, 0],
+                [1, 0, 0, 0]
+            ]
+    }
+}
+
+const getSearchMatrix = (n = 4, isHardMode) => 
+isHardMode ? getHardModeSearchMatrix() :
+new Array(n).fill(null).map((x, z) => {
+    let a = new Array(n).fill(0);
+    a[z] = 1
+    return a
+}).sort(() => Math.random() - 0.5)
+
+const getSearchCoords = (n = 4, isHardMode) => {
+    let arr = [];
+    const searchArr = getSearchMatrix(n, isHardMode)
+    for (let y = 0; y < 10; y++) {
+        for (let x = 0; x < 10; x++) {
+            if(searchArr[y%n][x%n] == 1) arr.push([x, y])
+        }
+    }
+
+    return arr
+}
+
+console.log(getSearchCoords(4, true).length)
